3c7c8f243d10f92e90f1c32ed036f60639fb4df1
murtalima <gabrielmurtalima99@gmail.com>
2019-09-13 04:08:49 -0300

    Add files via upload

---
+++ release/moc_mainwindow.cpp
@@ -0,0 +1,273 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../CompGrafica/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.13.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[44];
+    char stringdata0[782];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 22), // "on_leXc_2_valueChanged"
+QT_MOC_LITERAL(2, 34, 0), // ""
+QT_MOC_LITERAL(3, 35, 24), // "on_leRaio_2_valueChanged"
+QT_MOC_LITERAL(4, 60, 22), // "on_leYc_2_valueChanged"
+QT_MOC_LITERAL(5, 83, 24), // "on_btFrameBuffer_clicked"
+QT_MOC_LITERAL(6, 108, 16), // "on_btDDA_clicked"
+QT_MOC_LITERAL(7, 125, 22), // "on_btBresenham_clicked"
+QT_MOC_LITERAL(8, 148, 23), // "on_btCirculoPit_clicked"
+QT_MOC_LITERAL(9, 172, 31), // "on_cbDesenharPlano_stateChanged"
+QT_MOC_LITERAL(10, 204, 29), // "on_btCirculoBresenham_clicked"
+QT_MOC_LITERAL(11, 234, 31), // "on_btCirculoCoordenadas_clicked"
+QT_MOC_LITERAL(12, 266, 12), // "printarPonto"
+QT_MOC_LITERAL(13, 279, 1), // "X"
+QT_MOC_LITERAL(14, 281, 1), // "Y"
+QT_MOC_LITERAL(15, 283, 2), // "px"
+QT_MOC_LITERAL(16, 286, 7), // "linepen"
+QT_MOC_LITERAL(17, 294, 13), // "printarPontos"
+QT_MOC_LITERAL(18, 308, 19), // "std::vector<QPoint>"
+QT_MOC_LITERAL(19, 328, 6), // "pontos"
+QT_MOC_LITERAL(20, 335, 16), // "calculoBresenham"
+QT_MOC_LITERAL(21, 352, 2), // "X1"
+QT_MOC_LITERAL(22, 355, 2), // "Y1"
+QT_MOC_LITERAL(23, 358, 2), // "X2"
+QT_MOC_LITERAL(24, 361, 2), // "Y2"
+QT_MOC_LITERAL(25, 364, 15), // "Imprimir_Deltas"
+QT_MOC_LITERAL(26, 380, 10), // "calculoDDA"
+QT_MOC_LITERAL(27, 391, 20), // "on_leX2_valueChanged"
+QT_MOC_LITERAL(28, 412, 20), // "on_leY2_valueChanged"
+QT_MOC_LITERAL(29, 433, 20), // "on_leY1_valueChanged"
+QT_MOC_LITERAL(30, 454, 20), // "on_leX1_valueChanged"
+QT_MOC_LITERAL(31, 475, 22), // "on_leX1_2_valueChanged"
+QT_MOC_LITERAL(32, 498, 22), // "on_leY1_2_valueChanged"
+QT_MOC_LITERAL(33, 521, 22), // "on_leY2_2_valueChanged"
+QT_MOC_LITERAL(34, 544, 22), // "on_leX2_2_valueChanged"
+QT_MOC_LITERAL(35, 567, 20), // "on_leYc_valueChanged"
+QT_MOC_LITERAL(36, 588, 20), // "on_leXc_valueChanged"
+QT_MOC_LITERAL(37, 609, 22), // "on_leRaio_valueChanged"
+QT_MOC_LITERAL(38, 632, 22), // "on_leXc_3_valueChanged"
+QT_MOC_LITERAL(39, 655, 22), // "on_leYc_3_valueChanged"
+QT_MOC_LITERAL(40, 678, 24), // "on_leRaio_3_valueChanged"
+QT_MOC_LITERAL(41, 703, 28), // "on_sbQuadrantes_valueChanged"
+QT_MOC_LITERAL(42, 732, 24), // "on_sbXFrame_valueChanged"
+QT_MOC_LITERAL(43, 757, 24) // "on_sbYFrame_valueChanged"
+
+    },
+    "MainWindow\0on_leXc_2_valueChanged\0\0"
+    "on_leRaio_2_valueChanged\0"
+    "on_leYc_2_valueChanged\0on_btFrameBuffer_clicked\0"
+    "on_btDDA_clicked\0on_btBresenham_clicked\0"
+    "on_btCirculoPit_clicked\0"
+    "on_cbDesenharPlano_stateChanged\0"
+    "on_btCirculoBresenham_clicked\0"
+    "on_btCirculoCoordenadas_clicked\0"
+    "printarPonto\0X\0Y\0px\0linepen\0printarPontos\0"
+    "std::vector<QPoint>\0pontos\0calculoBresenham\0"
+    "X1\0Y1\0X2\0Y2\0Imprimir_Deltas\0calculoDDA\0"
+    "on_leX2_valueChanged\0on_leY2_valueChanged\0"
+    "on_leY1_valueChanged\0on_leX1_valueChanged\0"
+    "on_leX1_2_valueChanged\0on_leY1_2_valueChanged\0"
+    "on_leY2_2_valueChanged\0on_leX2_2_valueChanged\0"
+    "on_leYc_valueChanged\0on_leXc_valueChanged\0"
+    "on_leRaio_valueChanged\0on_leXc_3_valueChanged\0"
+    "on_leYc_3_valueChanged\0on_leRaio_3_valueChanged\0"
+    "on_sbQuadrantes_valueChanged\0"
+    "on_sbXFrame_valueChanged\0"
+    "on_sbYFrame_valueChanged"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      31,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  169,    2, 0x08 /* Private */,
+       3,    0,  170,    2, 0x08 /* Private */,
+       4,    0,  171,    2, 0x08 /* Private */,
+       5,    0,  172,    2, 0x08 /* Private */,
+       6,    0,  173,    2, 0x08 /* Private */,
+       7,    0,  174,    2, 0x08 /* Private */,
+       8,    0,  175,    2, 0x08 /* Private */,
+       9,    0,  176,    2, 0x08 /* Private */,
+      10,    0,  177,    2, 0x08 /* Private */,
+      11,    0,  178,    2, 0x08 /* Private */,
+      12,    4,  179,    2, 0x08 /* Private */,
+      17,    2,  188,    2, 0x08 /* Private */,
+      20,    5,  193,    2, 0x08 /* Private */,
+      26,    5,  204,    2, 0x08 /* Private */,
+      27,    0,  215,    2, 0x08 /* Private */,
+      28,    0,  216,    2, 0x08 /* Private */,
+      29,    0,  217,    2, 0x08 /* Private */,
+      30,    0,  218,    2, 0x08 /* Private */,
+      31,    0,  219,    2, 0x08 /* Private */,
+      32,    0,  220,    2, 0x08 /* Private */,
+      33,    0,  221,    2, 0x08 /* Private */,
+      34,    0,  222,    2, 0x08 /* Private */,
+      35,    0,  223,    2, 0x08 /* Private */,
+      36,    0,  224,    2, 0x08 /* Private */,
+      37,    0,  225,    2, 0x08 /* Private */,
+      38,    0,  226,    2, 0x08 /* Private */,
+      39,    0,  227,    2, 0x08 /* Private */,
+      40,    0,  228,    2, 0x08 /* Private */,
+      41,    0,  229,    2, 0x08 /* Private */,
+      42,    0,  230,    2, 0x08 /* Private */,
+      43,    0,  231,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::QPixmap, QMetaType::Int, QMetaType::Int, QMetaType::QPixmap, QMetaType::Int,   13,   14,   15,   16,
+    QMetaType::QPixmap, 0x80000000 | 18, QMetaType::QPixmap,   19,   15,
+    0x80000000 | 18, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Bool,   21,   22,   23,   24,   25,
+    0x80000000 | 18, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Int, QMetaType::Bool,   21,   22,   23,   24,   25,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_leXc_2_valueChanged(); break;
+        case 1: _t->on_leRaio_2_valueChanged(); break;
+        case 2: _t->on_leYc_2_valueChanged(); break;
+        case 3: _t->on_btFrameBuffer_clicked(); break;
+        case 4: _t->on_btDDA_clicked(); break;
+        case 5: _t->on_btBresenham_clicked(); break;
+        case 6: _t->on_btCirculoPit_clicked(); break;
+        case 7: _t->on_cbDesenharPlano_stateChanged(); break;
+        case 8: _t->on_btCirculoBresenham_clicked(); break;
+        case 9: _t->on_btCirculoCoordenadas_clicked(); break;
+        case 10: { QPixmap _r = _t->printarPonto((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< QPixmap(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4])));
+            if (_a[0]) *reinterpret_cast< QPixmap*>(_a[0]) = std::move(_r); }  break;
+        case 11: { QPixmap _r = _t->printarPontos((*reinterpret_cast< std::vector<QPoint>(*)>(_a[1])),(*reinterpret_cast< QPixmap(*)>(_a[2])));
+            if (_a[0]) *reinterpret_cast< QPixmap*>(_a[0]) = std::move(_r); }  break;
+        case 12: { std::vector<QPoint> _r = _t->calculoBresenham((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4])),(*reinterpret_cast< bool(*)>(_a[5])));
+            if (_a[0]) *reinterpret_cast< std::vector<QPoint>*>(_a[0]) = std::move(_r); }  break;
+        case 13: { std::vector<QPoint> _r = _t->calculoDDA((*reinterpret_cast< int(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2])),(*reinterpret_cast< int(*)>(_a[3])),(*reinterpret_cast< int(*)>(_a[4])),(*reinterpret_cast< bool(*)>(_a[5])));
+            if (_a[0]) *reinterpret_cast< std::vector<QPoint>*>(_a[0]) = std::move(_r); }  break;
+        case 14: _t->on_leX2_valueChanged(); break;
+        case 15: _t->on_leY2_valueChanged(); break;
+        case 16: _t->on_leY1_valueChanged(); break;
+        case 17: _t->on_leX1_valueChanged(); break;
+        case 18: _t->on_leX1_2_valueChanged(); break;
+        case 19: _t->on_leY1_2_valueChanged(); break;
+        case 20: _t->on_leY2_2_valueChanged(); break;
+        case 21: _t->on_leX2_2_valueChanged(); break;
+        case 22: _t->on_leYc_valueChanged(); break;
+        case 23: _t->on_leXc_valueChanged(); break;
+        case 24: _t->on_leRaio_valueChanged(); break;
+        case 25: _t->on_leXc_3_valueChanged(); break;
+        case 26: _t->on_leYc_3_valueChanged(); break;
+        case 27: _t->on_leRaio_3_valueChanged(); break;
+        case 28: _t->on_sbQuadrantes_valueChanged(); break;
+        case 29: _t->on_sbXFrame_valueChanged(); break;
+        case 30: _t->on_sbYFrame_valueChanged(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 31)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 31;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 31)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 31;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
